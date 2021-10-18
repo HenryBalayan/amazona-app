e5cd687026477e13287835667ac77fa66f1b797b
henri <henrikh111@outlook.com>
2021-10-18 23:03:41 +0400

    Implement-Add-To-Cart-Action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -24,8 +24,11 @@ export const detailsProduct = (productId) => async (dispatch) => {
     dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
     try {
       const { data } = await Axios.get(`/api/products/${productId}`);
+    
       dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
     } catch (error) {
+   
+     
       dispatch({
         type: PRODUCT_DETAILS_FAIL,
         payload:
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,20 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import {  cartReducers } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productListReducer';
 
 
-const initialState = {};
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+};
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails:productDetailsReducer
+  productDetails:productDetailsReducer,
+  cart:cartReducers
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
